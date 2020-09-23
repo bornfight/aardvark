1a5821ea7e6150f13beae79e6d7ef1fca2442e81
JuraJuki <cold.slayer@hotmail.com>
2020-09-23 07:40:20 +0200

    fix(jsonApiReducer): remove optional chaining

--- src/reducers/JsonApiReducer.ts
+++ src/reducers/JsonApiReducer.ts
@@ -94,7 +94,7 @@ export class JsonApiReducer {
             action.responseData,
             action.method,
         );
-        const dataMeta = action?.responseData?.meta;
+        const dataMeta = action.responseData.meta;
 
         const meta: MetaData = {
             [action.operation]: {
